670b0b4b22494cbc25e6bfe332f3adc35af63272
Jeong Seung Hwan <tmdghks5466@naver.com>
2017-08-18 14:14:00 +0900

    settings 배포준비;

--- helloeb1/settings.py
+++ helloeb1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'dd#7w39i!7%2%v3c-tn=nr5xm$#w7e%9+f(by%o@2kgu4e7d#*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -118,3 +118,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
